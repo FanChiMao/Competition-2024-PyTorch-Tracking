a73d8caa50b01071453eda2f1bf42a19a976491e
Chi-Mao <qaz5517359@gmail.com>
2024-06-01 14:07:34 +0800

    add code

--- Detector/yolov9_model_colab_usage/experimental.py
+++ Detector/yolov9_model_colab_usage/experimental.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 import numpy as np
 import torch
@@ -236,7 +237,7 @@ class End2End(nn.Module):
 
 def attempt_load(weights, device=None, inplace=True, fuse=True):
     # Loads an ensemble of models weights=[a,b,c] or a single model weights=[a] or weights=a
-    from models.yolo import Detect, Model
+    from Detector.yolov9.models.yolo import Detect, Model
 
     model = Ensemble()
     for w in weights if isinstance(weights, list) else [weights]:
